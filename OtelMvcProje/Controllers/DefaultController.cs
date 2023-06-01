62843c0790e045b172849a9c95032d26f2cc3cc3
SUMEYYE <SUMEYYE@ERKAN>
2023-06-01 15:24:25 +0300

    Proje dosyası ekle.

---
+++ OtelMvcProje/Controllers/DefaultController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using OtelMvcProje.Models.Entity;
+
+
+namespace OtelMvcProje.Controllers
+{
+
+    
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+        //DbOtelYeniEntities dbOtel = new DbOtelYeniEntities();
+        public ActionResult Hakkimizda()
+        {
+            
+            //var veriler = dbOtel.TblHakkimda.ToList();
+            return View();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialSosyalMedya()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ OtelMvcProje/Controllers/IletisimController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace OtelMvcProje.Controllers
+{
+    public class IletisimController : Controller
+    {
+        // GET: Iletisim
+        public ActionResult Index()
+        {
+
+
+            return View();
+        }
+    }
+}
